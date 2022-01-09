3f894441e779e822c3696a5a00a8b798b2c4466d
knameless27 <knameless27@gmail.com>
2022-01-09 18:41:27 -0500

    adding last changes

--- Juego1/js/app.js
+++ Juego1/js/app.js
@@ -1,12 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
     medidas.tmMedida();
     var a1 = new Hitbox(0, 90, 100, 100);
-    //medidas.dibujarTiles();
+    medidas.dibujarTiles();
+    teclas.iniciar();
 })
 
 var start = document.getElementById("start");
 start.addEventListener('click', function() {
     bucle.bucleR();
+});
+
+var start = document.getElementById("mostrar");
+start.addEventListener('click', function() {
+    teclas.mostrarTeclas();
 });
 
 window.addEventListener('resize', function() {
